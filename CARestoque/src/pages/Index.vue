38a92bb2ac0e398ef2eb0b7843b0a4c4b9c6d037
Lucas Rosario <LucasRosario@users.noreply.github.com>
2020-02-13 21:22:27 -0300

    Tela login feita

--- CARestoque/src/pages/Index.vue
+++ CARestoque/src/pages/Index.vue
@@ -1,39 +1,40 @@
 <template>
   <q-page class="flex flex-center">
 
-    <div class="q-pa-md" style="max-width: 400px">
+    <div class="q-pa-xl border-red">
 
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
-      <q-input
-        filled
-        v-model="name"
-        label="Your name *"
-        hint="Name and surname"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
-      />
+    <q-form @submit="onSubmit" @reset="onReset" class="q-pa-xl border-black">
+      <q-img
+        class="m-pa-auto border-red"
+        :src="url"
+        spinner-color="white"
+      ></q-img>
 
       <q-input
-        filled
-        type="number"
-        v-model="age"
-        label="Your age *"
-        lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'Please type your age',
-          val => val > 0 && val < 100 || 'Please type a real age'
-        ]"
-      />
-
-      <q-toggle v-model="accept" label="I accept the license and terms" />
+        class="q-pa-sm"
+        rounded outlined
+        v-model="email"
+        type="email"
+        label="Digite o e-mail">
+      </q-input>
+      <q-input
+      class="q-pa-sm"
+       rounded outlined
+       v-model="password"
+       :type="isPwd ? 'password' : 'text'"
+       label="Digite a senha">
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          ></q-icon>
+        </template>
+      </q-input>
 
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+        <q-btn label="Entrar" type="submit" color="secondary"/>
+        <q-btn label="Esqueci a senha" type="reset" color="secondary" flat class="q-ml-sm" />
       </div>
     </q-form>
 
@@ -41,16 +42,15 @@
 
   </q-page>
 </template>
-
 <script>
 export default {
   name: 'PageIndex',
   data () {
     return {
-      name: null,
-      age: null,
-
-      accept: false
+      url: '../statics/logo-car.png',
+      password: '',
+      isPwd: true,
+      email: ''
     }
   },
 
